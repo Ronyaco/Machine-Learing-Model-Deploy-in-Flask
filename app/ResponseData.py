b7e77364375af3670aa22cc9885bba4087b575e8
Rony Morales <65436006+Ronyaco@users.noreply.github.com>
2021-09-13 16:18:06 +1000

    Deleted files unused

--- app/ResponseData.py
+++
@@ -1,7 +0,0 @@
-class ResponseData:
-
-    def __init__(self, dataActual, dataPrediction, date):
-
-        self.dataActual = dataActual
-        self.dataPrediction = dataPrediction
-        self.date = date
